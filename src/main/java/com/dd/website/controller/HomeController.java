05166cea4d6c145e571bfb607f6981797c6c09cc
Darci K Saucedo <darcikhey27@gmail.com>
2020-04-25 18:31:46 -0700

    Added thymeleaf views

--- src/main/java/com/dd/website/controller/HomeController.java
+++ src/main/java/com/dd/website/controller/HomeController.java
@@ -8,11 +8,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-    private static final String HOME_VIEW = "root";
+    private static final String HOME_VIEW = "home";
+    private static final String ABOUT_VIEW = "about";
+    private static final String CONTACT_US_VIEW = "contact-us";
+    private static final String TEST_VIEW = "test";
+
+
     @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return HOME_VIEW;
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return ABOUT_VIEW;
+    }
+
+    @GetMapping("/contact")
+    public String contactUs() {
+        return CONTACT_US_VIEW;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return TEST_VIEW;
     }
 
 
